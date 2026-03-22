ae0037c2854f2af4a248e62242b895c248b6525c
zdl <3218992152@qq.com>
2026-03-22 23:37:54 +0800

    重新配置不使用ts语法

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
 import path from 'path'
 
 // https://vite.dev/config/
@@ -11,13 +12,15 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+
+  build: {
+    outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static
+    emptyOutDir: true,
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static
-    emptyOutDir: true,
   },
 })
